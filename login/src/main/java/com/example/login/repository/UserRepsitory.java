ba58d4325cdca259bf338bb383459ee45735df4d
de110 <skyyks98@gmail.com>
2022-08-06 16:39:54 +0900

    Revert "upload files"
    This reverts commit 646908b5d53ef7a2938c3fb193faa4c175083406.

--- login/src/main/java/com/example/login/repository/UserRepsitory.java
+++
@@ -1,9 +0,0 @@
-package com.example.login.repository;
-
-import com.example.login.domain.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepsitory extends JpaRepository<User, Long> {
-
-}
