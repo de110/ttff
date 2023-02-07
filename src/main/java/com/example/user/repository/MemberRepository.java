4937023e512e9b58524c7df264a4a3bfe35b054a
de110 <skyyks98@gmail.com>
2023-02-07 17:02:24 +0900

    remove files

--- src/main/java/com/example/user/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.user.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
--- src/main/java/com/example/user/service/AttendService.java
+++
@@ -1,5 +0,0 @@
-package com.example.user.service;
-
-public class AttendService {
-    
-}
