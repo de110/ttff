6d86b2cf9f90368ddc0f31d610d52e10b183def6
de110 <skyyks98@gmail.com>
2023-04-17 14:57:50 +0900

    test Board API Controller

---
+++ src/test/java/com/example/ttff/domain/BoardsApiControllerTest.java
@@ -0,0 +1,52 @@
+package com.example.ttff.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.ttff.dto.BoardDTO;
+import com.example.ttff.repository.BoardRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BoardsApiControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void Post_create() throws Exception {
+
+        // given
+        String title = "title8";
+        String rule = "rule8";
+
+        Board board = Board.builder().title(title).rule(rule).build();
+
+        String url = "http://localhost:" + port + "/board";
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, board,
+                Long.class);
+
+        // then
+
+        assertThat(responseEntity.getStatusCode());
+        List<Board> all = boardRepository.findAll();
+        assertThat(all.get(7).getTitle()).isEqualTo(title);
+        assertThat(all.get(7).getRule()).isEqualTo(rule);
+    }
+
+}
