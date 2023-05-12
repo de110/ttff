0b7bf56f752440ee13a465518bf0e593a6a8090d
de110 <skyyks98@gmail.com>
2023-05-12 17:20:38 +0900

    feat add builder to MemberDto

--- src/main/java/com/example/ttff/dto/MemberDto.java
+++ src/main/java/com/example/ttff/dto/MemberDto.java
@@ -1,8 +1,10 @@
 package com.example.ttff.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class MemberDto {
     private String memberId;
     private String password;
